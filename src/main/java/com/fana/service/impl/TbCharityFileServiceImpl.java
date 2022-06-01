e0d8d4451f25ad77cbdc171807c846d3477a4f94
pklaa <2746680956@qq.com>
2022-06-01 17:51:46 +0800

    p

---
+++ src/main/java/com/fana/service/impl/TbCharityFileServiceImpl.java
@@ -0,0 +1,20 @@
+package com.fana.service.impl;
+
+import com.fana.entry.pojo.TbCharityFile;
+import com.fana.mapper.TbCharityFileMapper;
+import com.fana.service.ITbCharityFileService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author astupidcoder
+ * @since 2022-06-01
+ */
+@Service
+public class TbCharityFileServiceImpl extends ServiceImpl<TbCharityFileMapper, TbCharityFile> implements ITbCharityFileService {
+
+}
