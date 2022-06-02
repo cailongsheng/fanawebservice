d8fb7e34ec79e3e0c5ad6716f61c9d75a580d77e
pklaa <2746680956@qq.com>
2022-06-02 15:11:29 +0800

    p

--- src/main/java/com/fana/service/impl/TbBannerServiceImpl.java
+++ src/main/java/com/fana/service/impl/TbBannerServiceImpl.java
@@ -1,5 +1,6 @@
 package com.fana.service.impl;
 
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.fana.config.ResponseResult;
 import com.fana.entry.pojo.TbBanner;
@@ -10,6 +11,7 @@ import com.fana.service.ITbBannerService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.fana.utils.FileUtils;
 import com.fana.utils.LogUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -30,14 +32,15 @@ public class TbBannerServiceImpl extends ServiceImpl<TbBannerMapper, TbBanner> i
     FileUtils fileUtils;
     @Resource
     TbBannerMapper bannerMapper;
-
+    @Value("${fana.ip}")
+    private String fanaIp;
     @Override
     public ResponseResult uploadBanner(List<BannerVo> banners) {
         banners.forEach(img->{
             try {
                 //上传
-                String uploadPath = fileUtils.upload(img.getFile(),"banner/");
-                bannerMapper.insert(TbBanner.builder().imageName(img.getImageName()).imagePath(uploadPath).build());
+                String uploadPath = fileUtils.uploadFile(img.getFile(),"banner");
+                bannerMapper.insert(TbBanner.builder().imageName(img.getImageName()).imagePath(fileUtils.getFileName(uploadPath)).build());
             } catch (IOException e) {
                 LogUtil.addErrorLog("上传图片文件异常", "upload/insert", "ip", e.getMessage().substring(0, 200));
                 throw  new CustomException(201, "File upload failed.");
@@ -49,6 +52,11 @@ public class TbBannerServiceImpl extends ServiceImpl<TbBannerMapper, TbBanner> i
 
     @Override
     public ResponseResult downLoadBanner() {
-        return ResponseResult.success(bannerMapper.selectList(new QueryWrapper<>()));
-    }
+        List<TbBanner> tbBanners = bannerMapper.selectList(new QueryWrapper<>());
+        tbBanners.stream().forEach(a->{
+            if(StrUtil.isNotBlank(a.getImagePath())) {
+                a.setImagePath(fanaIp + "banner/" + a.getImagePath());
+            }
+        });
+        return ResponseResult.success(tbBanners);    }
 }
